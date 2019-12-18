a245386f885b60a85a0509d4a62fea069129c58d
quendal <quendal@gmail.com>
2019-12-17 21:17:41 -0600

    Update auth.config.ts

--- projects/sample/src/app/auth.config.ts
+++ projects/sample/src/app/auth.config.ts
@@ -4,7 +4,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: 'https://steyer-identity-server.azurewebsites.net/identity',
+  issuer: 'https://identity-nirvana.auth.us-east-1.amazoncognito.com/authorize',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/index.html',
@@ -13,11 +13,11 @@ export const authConfig: AuthConfig = {
   silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
 
   // The SPA's id. The SPA is registerd with this id at the auth-server
-  clientId: 'spa-demo',
+  clientId: 'web1',
 
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
-  scope: 'openid profile email voucher',
+  scope: 'openid profile email',
 
   // silentRefreshShowIFrame: true,
 
